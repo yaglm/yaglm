5a8afcee774724a941f9dd6373ca7c6da4a13828
idc9 <idc9@cornell.edu>
2021-07-06 15:07:01 -0400

    up

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = ['numpy',
 
 
 setup(name='ya_glm',
-      version='0.0.1',
+      version='0.0.0',
       description='A flexible package for fitting penalized generalized linear models.',
       author='Iain Carmichael',
       author_email='idc9@cornell.edu',
