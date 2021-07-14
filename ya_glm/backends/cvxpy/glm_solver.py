6c8f65e159e1c29cb577c97a0aa206c8a87c023a
idc9 <idc9@cornell.edu>
2021-07-14 15:14:13 -0400

    added more penalties

--- ya_glm/backends/cvxpy/glm_solver.py
+++ ya_glm/backends/cvxpy/glm_solver.py
@@ -3,7 +3,8 @@ from functools import partial
 from time import time
 
 from ya_glm.utils import clip_zero
-from ya_glm.cvxpy.penalty import lasso_penalty, ridge_penalty, tikhonov_penalty
+from ya_glm.cvxpy.penalty import lasso_penalty, ridge_penalty,\
+     tikhonov_penalty,  multi_task_lasso_penalty, group_lasso_penalty
 from ya_glm.cvxpy.loss_functions import lin_reg_loss, log_reg_loss,\
     quantile_reg_loss
 from ya_glm.backends.fista.glm_solver import process_param_path
@@ -148,11 +149,6 @@ def setup_problem(X, y,
     """
     glm_loss = get_glm_loss(loss_func)
 
-    # TODO: add these
-    if groups is not None:
-        raise NotImplementedError
-    if L1to2:
-        raise NotImplementedError
     if nuc:
         raise NotImplementedError
 
@@ -185,13 +181,23 @@ def setup_problem(X, y,
 
     # Add lasso
     if lasso_pen is not None:
-        objective += lasso_pen * lasso_penalty(coef, weights=lasso_weights)
+
+        if groups is not None:
+            objective += lasso_pen * \
+                group_lasso_penalty(coef, groups=groups, weights=lasso_weights)
+
+        elif L1to2:
+            objective += lasso_pen * \
+                multi_task_lasso_penalty(coef, weights=lasso_weights)
+
+        else:
+            objective += lasso_pen * lasso_penalty(coef, weights=lasso_weights)
 
     # Add ridge
     if ridge_pen is not None:
         if tikhonov:
             objective += ridge_pen * \
-                tikhonov_penalty(coef, tikT_tik=tikhonov.T @ tikhonov)
+                tikhonov_penalty(coef, tikhonov=tikhonov)
 
         else:
             objective += ridge_pen * ridge_penalty(coef, weights=ridge_weights)
--- ya_glm/cvxpy/penalty.py
+++ ya_glm/cvxpy/penalty.py
@@ -15,12 +15,25 @@ def ridge_penalty(coef, weights=None):
         return 0.5 * cp.sum_squares(coef)
 
 
-def tikhonov_penalty(coef, tikT_tik):
-    """
-    Parameters
-    ----------
-    tik_tik: array-like, shape (n_features, n_features)
-        The squared tikhonov matrix tikT_tik = tikhonov.T @ tikhonov
+def tikhonov_penalty(coef, tikhonov):
+    return 0.5 * cp.quad_form(coef, tikhonov.T @ tikhonov)
 
-    """
-    return 0.5 * cp.quad_form(coef, tikT_tik)
+
+def multi_task_lasso_penalty(coef, weights=None):
+
+    row_norms = cp.norm(coef, p='fro', axis=1)
+
+    if weights:
+        return weights.T @ row_norms
+    else:
+        return cp.sum(row_norms)
+
+
+def group_lasso_penalty(coef, groups, weights=None):
+
+    group_norms = [cp.norm(coef[grp_idxs], p='fro') for grp_idxs in groups]
+
+    if weights is None:
+        return cp.sum(group_norms)
+    else:
+        return weights.T @ cp.hstack(group_norms)
