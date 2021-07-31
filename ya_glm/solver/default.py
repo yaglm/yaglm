54f5131a24bf55948bcec6bb513687cb951fb9fc
idc9 <idc9@cornell.edu>
2021-07-30 23:39:16 -0400

    avoid having to install cvxpy

--- ya_glm/solver/default.py
+++ ya_glm/solver/default.py
@@ -1,10 +1,12 @@
-from ya_glm.solver.QuantileLQProgSolver import QuantileLQProgSolver
 from ya_glm.solver.FistaSolver import FistaSolver
 
 
 def get_default_solver(loss, penalty=None):
 
     if loss.name == 'quantile':
+        # avoid manditory installation of cvxpy
+        from ya_glm.solver.QuantileLQProgSolver import QuantileLQProgSolver
+
         return QuantileLQProgSolver()
 
     else:
