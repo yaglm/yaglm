1c8275a81d24dc7d4d7a83b8fc50072c42c5a826
idc9 <idc9@cornell.edu>
2021-09-24 17:45:14 -0400

    updated for new init_data

---
+++ ya_glm/adaptive.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from ya_glm.opt.from_config.lla_structure import get_transform
+from ya_glm.config.base_penalty import get_flavor_info
+
+
+def get_adaptive_weights(coef_init, expon=1,
+                         pertub_init=None, n_samples=None,
+                         transform=None):
+    """
+    Computes the adaptive weights from an initial coefficient.
+
+    Parameters
+    ----------
+    coef_init: array-like
+        The initial cofficient used to set the adative weights.
+
+    perturb_init: str, float, None
+        (Optional) Perturbation to add to initial coefficeint e.g. to avoid dividing by zero. If pertub_init='n_samples', will use 1/n_samples.
+
+    n_samples: None, int
+        (Optional) The number of training samples used to estimatae the initial coefficient.
+
+    transform: None, callable(coef) -> array-like
+        (Optional) Transformation to apply to the coefficient before computing the adatpive weights. E.g. for the adaptive nuclear norm transform should output the singular values.
+
+    Output
+    ------
+    adpt_weights: array-like
+        The adaptive weights.
+    """
+    # check arguments
+    if pertub_init == 'n_samples':
+        assert n_samples is not None, \
+            "Must provide an value for n_samples"
+
+        pertub_init = 1 / n_samples
+
+    elif pertub_init is None:
+        pertub_init = 0.0
+
+    # possibly transform coef_init
+    if transform is None:
+        values = coef_init
+    else:
+        values = transform(coef_init)
+
+    # compute adaptiv weights
+    return 1 / (_abs(values) + pertub_init) ** expon
+
+
+def _abs(x):
+    """
+    Ensures the input is positive and a numpy array.
+    """
+    return abs(np.array(x, copy=False))
+
+
+def set_adaptive_weights(penalty, init_data):
+    """
+    Sets the adaptive weights for a penalty config from the initializer data.
+
+    Parameters
+    ----------
+    penalty: PenaltyConfig
+        The adaptive penalty config object whose weight we want to set. Note this config will be modified in place.
+
+    init_data: dict
+        The initializer data; must have keys 'coef_init', 'n_samples'.
+
+    Output
+    ------
+    penalty: PenaltyConfig
+        The penalty with the weights set to the adaptive weights.
+    """
+
+    # double check penalty is adaptive
+    flavor_type = get_flavor_info(penalty)
+    if flavor_type != 'adaptive':
+        raise ValueError("Attempting to set adaptive weights for"
+                         "non-adaptive penalty!")
+
+    # get the transformation
+    transform = get_transform(penalty)
+
+    adpt_weights = get_adaptive_weights(coef_init=init_data['coef'],
+                                        expon=penalty.flavor.expon,
+                                        pertub_init=penalty.flavor.pertub_init,
+                                        n_samples=init_data['n_samples'],
+                                        transform=transform)
+
+    penalty.set_params(weights=adpt_weights)
+
+    return penalty
---
+++ ya_glm/pen_max/non_convex.py
@@ -0,0 +1,49 @@
+
+def adjust_pen_max_for_non_convex(cvx_max_val, penalty, init_data):
+    """
+    Parameters
+    ----------
+    cvx_max_val: float
+        The largest reasonable penalty value for the convex problem.
+
+    penalty: PenaltyConfig
+
+    init_data: dict
+        The initializeation data. If we are using the LLA algorithm, this dict should have a key 'lla'.
+
+    Output
+    -----
+    pen_max: float
+        The possibly adjusted largest reasonable penalty parameter.
+    """
+
+    # check if we are using the LLA algorithm
+    lla = init_data['lla']
+
+    if lla:  # non-convex LLA
+
+        # TODO-THINK-THROUGH: this aweful local import
+        # gets around a circular import issue e.g. the from_config modules
+        # import from base_penalty, but base_penalty imports this module
+        # We should probably re-organize the code to get rid of this issue
+        from ya_glm.opt.from_config.penalty import get_nonconvex_func_from
+        from ya_glm.opt.from_config.lla_structure import get_transform
+
+        transf = get_transform(penalty)
+        pen_func = get_nonconvex_func_from(penalty)
+        coef_init = init_data['coef']
+
+        if not hasattr(pen_func, 'fcp_data'):
+            raise NotImplementedError("TODO Need to add FCP data for"
+                                      "this penalty")
+        a1 = pen_func.fcp_data['a1']
+        b1 = pen_func.fcp_data['b1']
+
+        return max(abs(transf(coef_init)).max() / b1, cvx_max_val / a1)
+
+    else:  # non-convex direct
+
+        if penalty.flavor.pen_func in ['mcp', 'scad']:
+            return cvx_max_val
+        else:
+            raise NotImplementedError("TODO add")
