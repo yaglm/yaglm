041da943c3e1b19f962193600cc2adc11b942c09
idc9 <idc9@cornell.edu>
2021-09-25 10:15:10 -0400

    added quadratic perturb

--- ya_glm/opt/prox.py
+++ ya_glm/opt/prox.py
@@ -21,12 +21,12 @@ def soft_thresh(vec, thresh_vals):
     return np.sign(vec) * np.fmax(abs(vec) - thresh_vals, 0)
 
 
-def prox_ridge_lasso(x, lasso_mult=1,
-                     lasso_weights=None, ridge_mult=1, ridge_weights=None):
+def prox_ridge_lasso(x, lasso_pen_val=1, lasso_weights=None,
+                     ridge_pen_val=1, ridge_weights=None, step=1):
     """
     Evaluates the proximal operator of
 
-    f(x) = lasso_mul * sum_j lasso_weights_j |x_j|
+    f(x; step) = lasso_mul * sum_j lasso_weights_j |x_j|
         + 0.5 * ridge_mul * sum_j lasso_weights_j x_j^2
 
     Parameters
@@ -34,34 +34,75 @@ def prox_ridge_lasso(x, lasso_mult=1,
     x: array-like
         The value at which to evaluate the prox operator.
 
-    lasso_mult: float
+    lasso_pen_val: float
         The multiplicative penalty value for the lasso penalty.
 
     lasso_weights: None, array-like
         The (optional) variable weights for the lasso penalty.
 
-    ridge_mult: float
+    ridge_pen_val: float
         The multiplicative penalty value for the ridge penalty.
 
     ridge_weights: None, array-like
         The (optional) variable weights for the ridge penalty.
 
+    step: float
+        The step size.
+
     Output
     ------
-    prox: array-like
+    prox_val: array-like
         The proximal operator.
     """
 
+    lasso_pen_val = lasso_pen_val * step
+    ridge_pen_val = ridge_pen_val * step
+
     if lasso_weights is None:
         lasso_weights = np.ones_like(x)
-    thresh = lasso_mult * np.array(lasso_weights)
+    thresh = lasso_pen_val * np.array(lasso_weights)
 
     if ridge_weights is None:
         ridge_weights = np.ones_like(x)
-    mult = ridge_mult * np.array(ridge_weights)
+    mult = ridge_pen_val * np.array(ridge_weights)
     mult = 1 / (1 + mult)
 
     return soft_thresh(x * mult, thresh * mult)
+
+
+def prox_ridge_perturb(x, prox, ridge_pen_val=1, step=1):
+    """
+    Evaluates the proximal operator of
+
+    f(x) + 0.5 * ridge_pen_val ||x||_2^2
+
+    e.g. see Theorem 6.13 of (Beck, 2017).
+
+    Parameters
+    ----------
+    x: array-like
+        The value at which to evaluate the prox operator.
+
+    prox: callable(x, step) -> array-like
+        The proximal operator of f.
+
+    ridge_pen_val: float
+        The ridge penalty value.
+
+    step: float
+        The step size.
+
+    Output
+    ------
+    prox_val: array-like
+        The proximal operator.
+
+    References
+    ----------
+    Beck, A., 2017. First-order methods in optimization. Society for Industrial and Applied Mathematics.
+    """
+    denom = ridge_pen_val * step + 1
+    return prox(x / denom, step=step / denom)
 
 
 def L2_prox(x, mult):
