556c1a5557c4a5c2572f194a836374116ff17e12
idc9 <idc9@cornell.edu>
2021-09-27 14:13:10 -0400

    block spearable

--- ya_glm/opt/BlockSeparable.py
+++ ya_glm/opt/BlockSeparable.py
@@ -1,7 +1,5 @@
 import numpy as np
-
 from ya_glm.opt.base import Func
-from ya_glm.opt.cat_utils import cat, decat, get_cat_block_idxs
 
 
 class BlockSeparable(Func):
@@ -12,45 +10,96 @@ class BlockSeparable(Func):
     
     where x_b denotes the bth block of x and the blocks are non-overlapping.
 
-    This object acts on the concatenated vector of all the blocks.
+    This object assumes the blocks blocks are groups of indices along the first axis (i.e. a concatenation with a possible shuffling).
 
     Parameters
     ----------
     funcs: list of Func
         The functions to apply to each block.
 
-    shapes: list of tuples
-        The shapes of the original blocks.
+    groups: list of lists
+        The indices of each block. Assumes the blocks are concatenated along the first axis of the input.
     """
-    
-    def __init__(self, funcs, shapes):
+    def __init__(self, funcs, groups):
         self.funcs = funcs
-        self.shapes = shapes
-        
-        # number of elements in each block
-        sizes = [np.prod(shape) for shape in shapes]
-        self.block_idxs = get_cat_block_idxs(sizes)
-        
+        self.groups = groups
+
+        # takes the consequitive concatenation of the blocks and maps
+        # them back to the proper order
+        self.sort_idxs = np.argsort(np.concatenate(self.groups))
+
+    def decat(self, x):
+        """
+        Generator yielding the blocks
+
+        Parameters
+        ----------
+        x: array-like
+            The concatenated blocks.
+
+        Yields
+        ------
+        block: array-like
+            Each block
+        """
+        for grp_idxs in self.groups:
+            yield x[grp_idxs]
+
+    def cat(self, blocks):
+        """
+        Concatenates the blocks into a vector.
+
+        Parameters
+        ----------
+        blocks: iterable
+            The blocks.
+
+        Output
+        ------
+        cat: array-like
+            The concatenated blocks
+        """
+        return np.concatenate(list(blocks))[self.sort_idxs]
+
     def _eval(self, x):
         # de-concatenate
-        blocks = decat(vec=x, block_idxs=self.block_idxs, shapes=self.shapes)
+        split_blocks = self.decat(x)
             
         # sum eval of each block
         return sum(self.funcs[b]._eval(block)
-                   for (b, block) in enumerate(blocks))
+                   for b, block in enumerate(split_blocks))
 
     def _grad(self, x):
         # de-concatenate
-        blocks = decat(vec=x, block_idxs=self.block_idxs, shapes=self.shapes)
+        split_blocks = self.decat(x)
         
         # concatenate grad of each block
-        return cat(self.funcs[b]._grad(block)
-                   for (b, block) in enumerate(blocks))
+        return self.cat(self.funcs[b]._grad(block)
+                        for b, block in enumerate(split_blocks))
 
     def _prox(self, x, step=1):
         # de-concatenate
-        blocks = decat(vec=x, block_idxs=self.block_idxs, shapes=self.shapes)
+        split_blocks = self.decat(x)
         
         # concatenate prox of each block
-        return cat(self.funcs[b]._prox(block, step=step)
-                   for (b, block) in enumerate(blocks))
+        return self.cat(self.funcs[b]._prox(block, step=step)
+                        for b, block in enumerate(split_blocks))
+
+    @property
+    def is_smooth(self):
+        # smooth if everyone is smooth
+        return all(f.is_smooth for f in self.funcs)
+
+    @property
+    def is_proximable(self):
+        # proximable if everyone is proximable
+        return all(f.is_proximable for f in self.funcs)
+
+    @property
+    def grad_lip(self):
+        lips = [f.grad_lip for f in self.funcs]
+        if any(lip is None for lip in lips):
+            return None
+        else:
+            # L-smoothness constant given by largest eval of hessian
+            return max(lips)
